c954e6a1f52984e66d086d2011582de5924a4831
Antonio González <agonzalezg86@gmail.com>
2021-06-18 19:14:40 +0200

    Initial commit

---
+++ src/__test__/index.test.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import request from "supertest";
+import app from "../index";
+
+describe("GET / - Demo api endpoint", () => {
+  it("Hello API request", async () => {
+    const result = await request(app).get("/");
+    expect(result.text).toContain("⚡");
+    expect(result.statusCode).toEqual(200);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv-safe";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// Common configs
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+// Server configs
+const app = express();
+app.use(morgan(process.env.MORGAN_MODE || "common"));
+app.use(cors());
+
+// Routes
+app.get("/", (_, res) => res.json({ message: "⚡ Express + TypeScript Server!" }));
+
+// Run server
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`⚡ Server is running on port ${port}`);
+});
+
+export default app;
